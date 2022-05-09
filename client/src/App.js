c2c258cd5ace4539083ca8b0fa785bb0588a2b68
Kat Imperial <imperialkatherine@gmail.com>
2022-05-09 15:02:25 -0400

    fixed react issues

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css';
+import Nav from './components/Nav'
+import Sidebar from './components/Sidebar'
+import Home from './components/Home'
+import Blog from './components/Blog'
+import TwigzMaker from './components/TwigzMaker'
+import Videos from './components/Videos'
+import Gallery from './components/Gallery';
+import LogIn from './components/LogIn'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+        <h1>hi</h1>
+      </header>
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+      <main>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/blog" element={ <Blog /> } />
+          <Route path="/twigzmaker" element={ <TwigzMaker /> } />
+          <Route path="/videos" element={ <Videos /> } />
+          <Route path="/gallery" element={ <Gallery /> } />
+          <Route path="/login" element={ <LogIn /> } />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
