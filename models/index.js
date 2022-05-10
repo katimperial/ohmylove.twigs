33d10bc14b88636e0cf0e5292aac96edd3e4817d
Kat Imperial <imperialkatherine@gmail.com>
2022-05-10 15:17:20 -0400

    seeded galleries and videos

--- models/index.js
+++ models/index.js
@@ -3,15 +3,18 @@ const BlogPostSchema = require('./blogPost')
 const CommentSchema = require('./comment')
 const UserSchema = require('./user')
 const GallerySchema = require('./gallery')
+const VideoSchema = require('./video')
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema)
 const Comment = mongoose.model('Comment', CommentSchema)
 const User = mongoose.model('User', UserSchema)
 const Gallery = mongoose.model('Gallery', GallerySchema)
+const Video = mongoose.model('Video', VideoSchema)
 
 module.exports = {
     BlogPost,
     Comment,
     User,
-    Gallery
+    Gallery,
+    Video
 }
