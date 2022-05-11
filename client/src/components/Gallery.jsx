29c1d26b0e4d1033ac06b9b4531981af3252f150
Kat Imperial <imperialkatherine@gmail.com>
2022-05-11 11:52:11 -0400

    mapped gallery images

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from 'react'
 
-const Gallery = () => {
+const Gallery = (props) => {
+
+    const [galleries, setGalleries] = useState()
+
+    const getGalleries = async () => {
+        try {
+            await axios.get(`http://localhost:3001/api/gallery`).then(
+                response =>
+                setGalleries(response.data.gallery))
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getGalleries()
+    }, [])
+
+    return(
+        <div>
+            {galleries ?
+                galleries.map((gallery) => (
+                    <div key={gallery.id} className="gallery">
+                        <img src={gallery.image} className="galleryImage" />
+                        <h4>{gallery.photographer}</h4>
+                    </div>
+                )) : null
+            }
+        </div>
+    )
 
 }
 
