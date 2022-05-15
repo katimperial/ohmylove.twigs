c973a977ad806545492348a923da7a06b0c0851d
Kat Imperial <imperialkatherine@gmail.com>
2022-05-15 19:29:47 -0400

    videos styling

--- client/src/components/Videos.jsx
+++ client/src/components/Videos.jsx
@@ -20,17 +20,19 @@ const Videos = () => {
     }, [])
 
     return(
-        <div>
+        <div className="Videos">
             <h1 className='header-text'>Videos</h1>
+            <div className="video">
             {videos ?
                 videos.map((videos) => (
-                    <div key={videos.id} className="video">
-                        <iframe height="auto" width="auto"  
+                    <div key={videos.id}>
+                        <iframe height="auto" width="auto" style={{display: 'block', maxWidth:'80%'}} className="indv-video"
                             src={videos.video}>
                         </iframe>
                     </div>
                 )) : null
             }
+            </div>
         </div>
     )
 
