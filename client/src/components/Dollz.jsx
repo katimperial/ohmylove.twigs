2d6aa27a47ce3b36366aadb04c456363faeb5c37
Kat Imperial <imperialkatherine@gmail.com>
2022-05-16 00:14:09 -0400

    got doll maker to work sort of..still some bugs

--- client/src/components/Dollz.jsx
+++ client/src/components/Dollz.jsx
@@ -2,25 +2,27 @@ import React from "react";
 
 const Dollz = (props) => {
 
-    const dragStart = e => {
-        const target = e.target;
+    // const dragStart = e => {
+    //     const target = e.target;
 
-        e.dataTransfer.setData('image_id', target.id);
+    //     e.dataTransfer.setData('image_id', target.id);
 
-        setTimeout(() => {
-            target.style.display = "block";
-        }, 0);
-    }
+    //     setTimeout(() => {
+    //         target.style.display = "block";
+    //     }, 0);
+    // }
 
-    const dragOver = e => {
-        e.stopPropagation();
-    }
+    // const dragOver = e => {
+    //     e.stopPropagation();
+    // }
 
     return (
-        <div id={props.id} className={props.className} draggable={props.draggable} onDragStart={dragStart} onDragOver={dragOver}>
+        <div id={props.id} className={props.className} draggable={props.draggable}>
             { props.children }
         </div>
     )
 }
 
 export default Dollz
+
+// onDragStart={dragStart} onDragOver={dragOver}
--- client/src/components/TwigzMaker.jsx
+++ client/src/components/TwigzMaker.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Maker from './Maker'
 import Dollz from './Dollz'
 
 const TwigzMaker = () => {
 
+    const [dressupState, setDressupState] = useState({
+        hair: {current: 0, total: 3},
+        clothes: {current: 0, total: 4},
+        hats: {current: 0, total: 2}
+    })
+
+    function next(item){
+        let next_num = dressupState[item].current + 1
+        let new_current = next_num < dressupState[item].total ? next_num : 0
+        setDressupState({
+            ...dressupState,
+            [item]: {
+                current: dressupState[item].current = new_current,
+                total: dressupState[item].total
+            }
+        })
+    }
+
 
     return (
         <div>
             <h1 className='header-text'>TwigzMaker</h1>
-            <main className='flexbox'>
+            <div id="container">
+                <div id="background">
+                    <div id="twigz"></div>
+                    <div id="clothes" className={"clothes"+(dressupState["clothes"].current+1)}></div>
+                    <div id="hair" className={"hair"+(dressupState["hair"].current+1)}></div>
+                    <div id="hat" className={"hats"+(dressupState["hats"].current+1)}></div>
+                    {/* <div id="buns-hair" className="hair2"></div>
+                    <div id="red-streaks-hair" className="hair3"></div> */}
+                    {/* <div id="boucher-outfit" className="clothes1"></div>
+                    <div id="hat-outfit" className="clothes2"></div>
+                    <div id="pendulum-pants" className="clothes3"></div>
+                    <div id="stage-outfit" className="clothes4"></div>
+                    <div id="stage-outfit-hat" className="hat1"></div>
+                    <div id="tricorner-hat" className="hat2"></div> */}
+                </div>
+                <div className='all-twigz-buttons'>
+                <button id="next-hair" className='twigz-button' onClick={() => next("hair")}>Next Hair</button>
+                <button id="next-outfit" className='twigz-button' onClick={() => next("clothes")}>Next Outfit</button>
+                <button id="next-hat" className='twigz-button' onClick={() => next("hat")}>Next Hat</button>
+                </div>
+            </div>
+            {/* <main className='flexbox'>
                 <Maker id="makerLeft" className="maker">
                     <Dollz id="twigz" className="doll" draggable="false">
                         <img src="https://i.imgur.com/Bh6zcpw.png"/>
@@ -43,7 +82,7 @@ const TwigzMaker = () => {
                         <img src="https://i.imgur.com/ZJVt6k6.png"/>
                     </Dollz>
                 </Maker>
-            </main>
+            </main> */}
         </div>
     )
 }
