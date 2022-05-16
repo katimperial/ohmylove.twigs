cd831821dcfc8277caf49bb0a24887f7d9982699
Kat Imperial <imperialkatherine@gmail.com>
2022-05-16 01:41:22 -0400

    sidebar styling

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -5,12 +5,15 @@ const Sidebar = () => {
     return (
         <div className='sidebarTotal'>
             <div className='sidebar'>
-            <a href="https://www.youtube.com/c/fkatwigs">
-                YouTube
-            </a>
-            <a href="https://www.fkatwigsofficial.com">
-                fkatwigsofficial.com
-            </a>
+                <a href="https://www.fkatwigsofficial.com" className='sidebar-link'>
+                    <img src="https://i.imgur.com/xpo90aE.png"/>
+                </a>
+                <a href="https://www.youtube.com/c/fkatwigs" className='sidebar-link'>
+                    <img src="https://i.imgur.com/aJncxpo.gif"/>
+                </a>
+                <a href="https://store.fkatwigsofficial.com/" className="store-link">
+                    Store
+                </a>
             </div>
         </div>
     )
