c0452321c7ed54b4c30edad8f2f4a2bdb0a50927
Kat Imperial <imperialkatherine@gmail.com>
2022-05-15 22:31:34 -0400

    some App and home styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div className='Home'>
-            <h1 className='header-text'>Welcome to ohmylove.twigs ♡</h1>
+            <h1 className='header-text'>Welcome! ♡</h1>
         </div>
     )
 
