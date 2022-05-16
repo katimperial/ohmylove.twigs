9e9d8babcca58238b073aad18be24533543853ef
Kat Imperial <imperialkatherine@gmail.com>
2022-05-16 01:49:30 -0400

    more sidebar styling

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Sidebar = () => {
     return (
         <div className='sidebarTotal'>
-            <div className='sidebar'>
+            <div className='side-info'>
                 <a href="https://www.fkatwigsofficial.com" className='sidebar-link'>
                     <img src="https://i.imgur.com/xpo90aE.png"/>
                 </a>
